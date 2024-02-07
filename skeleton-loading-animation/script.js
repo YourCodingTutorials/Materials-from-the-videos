cd46c74ff98dc1955a808a04bd8510ae96a0b669
Your Coding Tutorials <155894361+YourCodingTutorials@users.noreply.github.com>
2024-02-07 20:46:12 +0700

    Create script.js
    JS for Skeleton Loading Animation was created

---
+++ skeleton-loading-animation/script.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+    var skeletonBlock = document.getElementById('skeletonBlock');
+    var textBlock = document.getElementById('textBlock');
+    skeletonBlock.style.display = 'none';
+    textBlock.style.display = 'flex';
+}, 3000);
